17eea9772d2b2185c4f598cfc9bd9dbc73a2a585
Arun Kumar R <aruntutter19@gmail.com>
2024-01-14 22:17:25 +0530

    TMDB - api stored in variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,6 @@
 "use strict";
 
-// global variables
-
-// functions
-const init = () => {};
-
-//event listeners
-
-// initial settings
-init();
+// API
+const api_Key = "api_key=f741ac48cd013b413e892b2a09ccf12c";
+const base_Url = "https://api.themoviedb.org/3";
+const api_Url = base_Url + "/discover/movie?sort_by=popularity.desc&" + api_Key;
